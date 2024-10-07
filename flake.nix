bf7dd0241d5f8d81f474fc68c03efa36f8415736
Tom Aisthorpe <tomaisthorpe@gmail.com>
2024-10-07 13:33:25 +0100

    chore: update to 24.05

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Tom's NixOS Flake";
 
   inputs = {
-    # Official NixOS package source, using nixos-23.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    # Official NixOS package source, using nixos-24.05 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     # home-manager, used for managing user configuration
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
