eff7bfb17e1a46f8bdb6c7e3e14f101f40ba979a
Tom Aisthorpe <tom.aisthorpe@aikido.dev>
2025-10-01 10:57:59 +0100

    feat(darwin): tmux setup

--- home/darwin.nix
+++ home/darwin.nix
@@ -3,4 +3,20 @@
   home.packages = with pkgs; [
     gnupg
   ];
+
+  programs.zsh = {
+    enable = true;
+    history.size = 10000;
+
+    oh-my-zsh = { 
+      enable = true;
+      plugins = [ "git" ];
+      theme = "robbyrussell";
+    };
+
+    shellAliases = {
+      j = "z";
+      gap = "git add --patch .";
+    };
+  };
 }
--- home/terminal.nix
+++ home/terminal.nix
@@ -43,7 +43,41 @@
     nix-direnv.enable = true;
   };
 
-  programs.tmux.enable = true;
+  programs.tmux = {
+    enable = true;
+    shortcut = "a";
+    shell = "${pkgs.zsh}/bin/zsh";
+
+    plugins = with pkgs; [
+      tmuxPlugins.sensible
+      { 
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavor 'mocha'
+          set -g @catppuccin_window_status_style "rounded"
+          set -g @catppuccin_window_current_text " #W"
+          set -g @catppuccin_window_text " #W"
+        '';
+      }
+      tmuxPlugins.session-wizard
+      tmuxPlugins.resurrect
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = "set -g @continuum-restore 'on'";
+      }
+    ];
+
+    mouse = true;
+
+    extraConfig = ''
+      set -g status-right ""
+      set -g default-terminal "tmux-256color"
+      set -g default-command "/bin/zsh"
+
+      bind -r "<" swap-window -d -t -1
+      bind -r ">" swap-window -d -t +1
+    '';
+  };
 
   home.packages = with pkgs; [
     fzf
